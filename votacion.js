15372f2ce7e22fbc3ca2a0f607747cbef00f5552
Brandon <brandon@raidho.mx>
2021-10-19 11:09:53 -0500

    fetching voting data

--- votacion.js
+++ votacion.js
@@ -1,9 +1,5 @@
 window.addEventListener("load", function () {
-    fetch("https://bienalnuevoleon.com/wp-json/wp/v2/estudiantil", {
-        method: "GET",
-        mode: "no-cors",
-        credentials: "omit",
-    })
+    fetch("https://bienalnuevoleon.com/wp-json/wp/v2/estudiantil")
         .then((response) => response.json())
         .then((data) => console.log(data))
         .catch((error) => console.log(error));
