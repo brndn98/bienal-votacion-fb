4629bd09ed12f970343e3200fae195b6a5791dc6
Brandon <brandon@raidho.mx>
2021-10-19 11:43:57 -0500

    fetching voting data

--- votacion.js
+++ votacion.js
@@ -13,12 +13,12 @@ window.addEventListener("load", function () {
                               cat.id, cat.slug;
                           })
                         : false;
-                fetchProfesional(categories);
+                fetchProfesional(base, categories);
             })
             .catch((error) => console.log(error));
     }
 
-    function fetchProfesional(categories) {
+    function fetchProfesional(base, categories) {
         fetch(base + "profesional?per_page=15")
             .then((res) => res.json())
             .then((data) => {
@@ -28,12 +28,12 @@ window.addEventListener("load", function () {
                               post.id, post.title.rendered, post.categoria_curatorial;
                           })
                         : false;
-                fetchEstudiantil(categories, profesionales);
+                fetchEstudiantil(base, categories, profesionales);
             })
             .catch((error) => console.log(error));
     }
 
-    function fetchEstudiantil(categories, profesionales) {
+    function fetchEstudiantil(base, categories, profesionales) {
         fetch(base + "estudiantil?per_page=15")
             .then((res) => res.json())
             .then((data) => {
