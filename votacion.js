0e2b253bde5c164a131a33e43f2c262c31483d1b
Brandon <brandon@raidho.mx>
2021-10-20 08:38:13 -0500

    finishing voting structure

--- votacion.js
+++ votacion.js
@@ -121,15 +121,47 @@ window.addEventListener("load", function () {
         var userInfo = document.createElement("div");
         userInfo.className = "mv-m";
         var userTitle = document.createElement("p");
-        userTitle.className = "t-12 c-gray";
+        userTitle.className = "t-14 c-gray";
         userTitle.textContent = "Usuario";
         var userId = document.createElement("p");
         userId.className = "mh-s";
         userId.textContent = capture.user;
         userInfo.appendChild(userTitle);
         userInfo.appendChild(userId);
-
         captureContainer.appendChild(userInfo);
+
+        var voteInfo = document.createElement("div");
+        voteInfo.className = "mv-m";
+        var voteTitle = document.createElement("p");
+        voteTitle.className = "t-14 c-gray";
+        voteTitle.textContent = "Votos";
+        voteInfo.appendChild(voteTitle);
+
+        for (var key in capture.vote) {
+            var voteCategory = document.createElement("div");
+            voteCategory.className = "mv-s mh-s";
+            var categoryTitle = document.createElement("p");
+            categoryTitle.className = "t-14";
+            categoryTitle.textContent = key;
+            voteCategory.appendChild(categoryTitle);
+            for (var type in capture.vote[key]) {
+                var cat = document.createElement("div");
+                cat.className = "mh-s";
+                var catTitle = document.createElement("p");
+                catTitle.className = "t-14 c-gray";
+                catTitle.textContent = type;
+                var catText = document.createElement("p");
+                catText.className = "mh-s";
+                catText.textContent = capture.vote[key][type] === false ? "Sin seleccionar" : capture.vote[key][type];
+
+                cat.appendChild(catTitle);
+                cat.appendChild(catText);
+                voteCategory.appendChild(cat);
+            }
+            voteInfo.appendChild(voteCategory);
+        }
+
+        captureContainer.appendChild(voteInfo);
     }
 
     function existsInArray(array, find) {
